3da2c04c0a1e9d9f69b0027a82d336c207ceddc4
oohasri <oohasri25@gmail.com>
2019-09-24 17:12:17 -0700

    Added templates

--- apps/order/models.py
+++ apps/order/models.py
@@ -5,6 +5,7 @@ class Restaurant(models.Model):
     email = models.CharField(max_length=100)
     location = models.CharField(max_length=100)
     phone_number = models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
--- apps/order/views.py
+++ apps/order/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import *
+
+def index(request):
+	return render(request, "order/index.html")
+
 
 # Create your views here.
